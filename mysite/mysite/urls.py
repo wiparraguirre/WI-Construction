ecac6cc70aa0071a873312ffe451b1f06d4630ec
Willy Iparraguirre <iparraguirrew@gmail.com>
2021-12-13 00:25:22 +0000

    Update

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 from django.conf.urls import include
 
 urlpatterns = [
-    path('chat/', include('chat.urls')),
+    path('chat/', include("chat.urls")),
     path('admin/', admin.site.urls),
     path('', include("myapp.urls")), 
 
